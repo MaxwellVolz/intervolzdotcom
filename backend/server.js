4a7b8cb35cbcc7c55df4b7a6c93a0568ea509c81
Maxwell Volz <maxwellvolz@gmail.com>
2024-05-27 11:09:59 -0700

    backend update with cors

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const path = require('path');
-// const cors = require('cors');
-
+const cors = require('cors');
 const app = express();
-const port = 5000;
 
-// Serve static files from the data directory
-app.use('/data', express.static(path.join(__dirname, 'data')));
+// Use the CORS middleware
+app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Catch-all handler to serve React's index.html for any other routes
+// Serve static files from the public directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Server is running on port ${port}`);
