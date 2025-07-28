4269d9af1dd633be81152e7f146fae9685f9bb52
maxwell <maxwellvolz@gmail.com>
2025-07-28 13:58:44 -0700

    trailing slash for routing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-  
+    trailingSlash: true,
     webpack(config) {
       config.module.rules.push({
         test: /\.svg$/,
