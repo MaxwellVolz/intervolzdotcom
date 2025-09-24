68856df861aa432e6b9ac33572aa4c8f33d210f8
maxwellvolz <maxwellvolz@gmail.com>
2025-09-23 21:10:42 -0700

    relatively annoyed with links

---
+++ public/wassuh/_next/static/chunks/26e5c48820886965.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/5142601f190dbe3f.js",
+  "static/chunks/turbopack-e0aeeb32b283ee9e.js"
+])
---
+++ public/wassuh/_next/static/yDQlayuC-JU6bVwxmOtei/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/1f486191d789ecce.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
