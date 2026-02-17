eb6e9f14c18c174805d8722bcf4f4cb664f76d30
maxwell <maxwellvolz@gmail.com>
2026-02-17 12:29:40 -0800

    article updates and ganalytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const GA_ID = 'G-D0Q7TRRTCS';
+
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
